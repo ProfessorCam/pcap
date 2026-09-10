721ac348be8e5774588718163887186f7d45a237
Cameron Ryan <cameron.ryan@outlook.com>
2026-09-10 12:09:25 -0600

    Site menu: link Packet Forensics

--- site/lessons.js
+++ site/lessons.js
@@ -34,7 +34,8 @@ var SITE = {
   menu: [
     { label: 'Frames & Packets', href: 'https://professorcam.github.io/frames/' },
     { label: 'Protocols', href: '#', current: true },
-    { label: 'Encryption and Protocols', href: 'https://professorcam.github.io/encryption/' }
+    { label: 'Encryption and Protocols', href: 'https://professorcam.github.io/encryption/' },
+    { label: 'Packet Forensics', href: 'https://professorcam.github.io/forensics/' }
   ]
 };
 
