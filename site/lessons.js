6751bca4f53d5bbb2f90f7cbdf27b0430e59fa74
Cameron Ryan <cameron.ryan@outlook.com>
2026-09-23 20:28:32 -0600

    Sites menu: add IPv4 Subnetting

--- site/lessons.js
+++ site/lessons.js
@@ -36,7 +36,8 @@ var SITE = {
     { label: 'Protocols', href: '#', current: true },
     { label: 'Encryption and Protocols', href: 'https://professorcam.github.io/encryption/' },
     { label: 'Packet Forensics', href: 'https://professorcam.github.io/forensics/' },
-    { label: 'Server Basics', href: 'https://professorcam.github.io/servers/' }
+    { label: 'Server Basics', href: 'https://professorcam.github.io/servers/' },
+    { label: 'IPv4 Subnetting', href: 'https://professorcam.github.io/ipv4/' }
   ]
 };
 
